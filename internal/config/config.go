55c436bfaf1ffac583c665fd80c85cffb8220a6f
stupidcabbage <vanya@gutche.ru>
2025-02-09 19:11:32 +0500

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,7 +22,7 @@ type AppConfig struct {
 func InitConfig() {
 	Config = AppConfig{
 		PostgresConnectionString: fmt.Sprintf("host=%s port=%s user=%s "+
-			"password=%s dbname=%s sslmode=require",
+			"password=%s dbname=%s sslmode=disable",
 			os.Getenv("DB_HOST"),
 			os.Getenv("DB_PORT"),
 			os.Getenv("DB_USER"),
